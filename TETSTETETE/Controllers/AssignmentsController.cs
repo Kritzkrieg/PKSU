8b166885ae0d35dd9bee6594eae05c92e0daeaa4
Henrik Hoeg <hotdacore@hotmail.com>
2012-04-18 11:27:18 +0200

    Added Assignment model Added AssignmentController Added Assignment CRUD views

---
+++ TETSTETETE/Controllers/AssignmentsController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TETSTETETE.Models;
+
+namespace TETSTETETE.Controllers
+{ 
+    public class AssignmentsController : Controller
+    {
+        private MySqlConnection db = new MySqlConnection();
+
+        //
+        // GET: /Assignments/
+
+        public ViewResult Index()
+        {
+            return View(db.Assignments.ToList());
+        }
+
+        //
+        // GET: /Assignments/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Assignment assignment = db.Assignments.Find(id);
+            return View(assignment);
+        }
+
+        //
+        // GET: /Assignments/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Assignments/Create
+
+        [HttpPost]
+        public ActionResult Create(Assignment assignment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Assignments.Add(assignment);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(assignment);
+        }
+        
+        //
+        // GET: /Assignments/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Assignment assignment = db.Assignments.Find(id);
+            if (assignment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(assignment);
+        }
+        //
+        // POST: /Assignments/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Assignment assignment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(assignment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(assignment);
+        }
+
+
+
+        //
+        // GET: /Assignments/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Assignment assignment = db.Assignments.Find(id);
+            return View(assignment);
+        }
+
+        //
+        // POST: /Assignments/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Assignment assignment = db.Assignments.Find(id);
+            db.Assignments.Remove(assignment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TETSTETETE/Models/Assignment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TETSTETETE.Models
+{
+    public class Assignment
+    {
+        public int ID { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class MySqlConnection : DbContext
+    {
+        public DbSet<Assignment> Assignments { get; set; }
+    }
+
+
+}
