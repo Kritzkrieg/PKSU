d42535f93b35339bdc07ed3005e2855309593a5d
Henrik Hoeg <hotdacore@hotmail.com>
2012-04-19 20:43:52 +0200

    Added Userlist

---
+++ TETSTETETE/Controllers/UserListController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using ELearning.Models;
+
+namespace ELearning.Controllers
+{
+    public class UserListController : Controller
+    {
+        //
+        // GET: /UserList/
+
+        private MySqlConnection db = new MySqlConnection();
+        
+        
+        public ActionResult Index()
+        {
+            var users = Membership.GetAllUsers();
+            return View(users);
+        }
+
+        public ActionResult UserProfile(string UserName = "0")
+        {
+            MembershipUser user = Membership.GetUser(UserName);
+            return View(user);
+        }
+
+    }
+}
