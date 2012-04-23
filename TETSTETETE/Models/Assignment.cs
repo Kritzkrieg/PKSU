f57787b061a606278409199a5b5f47547c141638
Henrik Hoeg <hotdacore@hotmail.com>
2012-04-23 16:18:24 +0200

    Updated CreateStudentAssignmentsDB script
    Added more attributes in Assignment(model)
    Updated CreateAssignmentsDB script with new attributes

--- TETSTETETE/Models/Assignment.cs
+++ TETSTETETE/Models/Assignment.cs
@@ -10,6 +10,11 @@ namespace ELearning.Models
     {
         public int ID { get; set; }
         public string Text { get; set; }
+        public string Option1 { get; set; }
+        public string Option2 { get; set; }
+        public string Option3 { get; set; }
+        public string Option4 { get; set; }
+        public string TrueOption { get; set; }
     }
 
     public class MySqlConnection : DbContext
