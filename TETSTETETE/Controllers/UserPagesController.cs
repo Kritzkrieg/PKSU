10560ef65805eddb57d8bef7fd50426cc4dc53b0
Henrik Hoeg <hotdacore@hotmail.com>
2012-04-23 11:30:19 +0200

    Added user-specific userpage UserProfile automatically grabs a MembershipUser from string = "UserName"
    SECURITY: Anyone can access any userpage via URL
    	SOLUTIONS:
    		1.Make UserProfile controller [POST]
    		2. Implement check: If user is not admin and does not own userpage, deny access

---
+++ TETSTETETE/Controllers/UserPagesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using ELearning.Models;
+
+namespace ELearning.Controllers
+{
+    public class UserPagesController : Controller
+    {
+        //
+        // GET: /UserPages/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UserProfile(string UserName = "0")
+        {
+            MembershipUser user = Membership.GetUser(UserName);
+            return View(user);
+        }
+
+    }
+}
