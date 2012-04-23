77f6b7b2808953ae5bc7e942b011fa43b7f12e7a
unknown <Thomas@TBO.(none)>
2012-04-23 14:03:16 +0200

    Assignment ting

---
+++ TETSTETETE/Controllers/AssignmentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ELearning.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        //
+        // GET: /Assignment/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
