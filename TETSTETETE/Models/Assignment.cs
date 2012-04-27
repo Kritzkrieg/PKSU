1a56b9ef6bf5e84c90c2cfceb46999a9010c27f3
unknown <Kim@Kim-THINK.(none)>
2012-04-27 14:39:28 +0200

    Formal changes. Linguistic errors fixed.

--- TETSTETETE/Models/Assignment.cs
+++ TETSTETETE/Models/Assignment.cs
@@ -14,18 +14,35 @@ namespace ELearning.Models
         [Display(Name = "Opgaveformulering")]
         public string Text { get; set; }
 
+        /*----------Teori----------*/
+
+        [Display(Name = "Teori 1")]
+        public string tOption1 { get; set; }
+
+        [Display(Name = "Teori 2")]
+        public string tOption2 { get; set; }
+
+        [Display(Name = "Teori 3")]
+        public string tOption3 { get; set; }
+
+        [Display(Name = "Teori 4")]
+        public string tOption4 { get; set; }
+
+        public string tTrueOption { get; set; }
+        public int TheoryAnswer { get; set; }
+
         /*----------Mellemregning----------*/
 
-        [Display(Name = "Svarmulighed 1")]
+        [Display(Name = "Mellemregning 1")]
         public string Option1 { get; set; }
 
-        [Display(Name = "Svarmulighed 2")]
+        [Display(Name = "Mellemregning 2")]
         public string Option2 { get; set; }
 
-        [Display(Name = "Svarmulighed 3")]
+        [Display(Name = "Mellemregning 3")]
         public string Option3 { get; set; }
 
-        [Display(Name = "Svarmulighed 4")]
+        [Display(Name = "Mellemregning 4")]
         public string Option4 { get; set; }
 
         public string TrueOption { get; set; }
@@ -33,37 +50,20 @@ namespace ELearning.Models
 
         /*----------Facit----------*/
 
-        [Display(Name = "Svarmulighed 1")]
+        [Display(Name = "Svar 1")]
         public string fOption1 { get; set; }
 
-        [Display(Name = "Svarmulighed 2")]
+        [Display(Name = "Svar 2")]
         public string fOption2 { get; set; }
 
-        [Display(Name = "Svarmulighed 3")]
+        [Display(Name = "Svar 3")]
         public string fOption3 { get; set; }
 
-        [Display(Name = "Svarmulighed 4")]
+        [Display(Name = "Svar 4")]
         public string fOption4 { get; set; }
 
         public string fTrueOption { get; set; }
         public int FinalAnswer { get; set; }
-
-        /*----------Teori----------*/
-
-        [Display(Name = "Svarmulighed 1")]
-        public string tOption1 { get; set; }
-
-        [Display(Name = "Svarmulighed 2")]
-        public string tOption2 { get; set; }
-
-        [Display(Name = "Svarmulighed 3")]
-        public string tOption3 { get; set; }
-
-        [Display(Name = "Svarmulighed 4")]
-        public string tOption4 { get; set; }
-        
-        public string tTrueOption { get; set; }
-        public int TheoryAnswer { get; set; }
     }
 
     public class MySqlConnection : DbContext
