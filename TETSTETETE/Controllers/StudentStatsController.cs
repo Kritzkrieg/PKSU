7d2059d2ba1f7e6b89d5a661c05dfb597618bb74
Henrik Hoeg <hotdacore@hotmail.com>
2012-05-09 16:03:52 +0200

    Stats

---
+++ TETSTETETE/Controllers/StudentStatsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ELearning.Models;
+
+namespace ELearning.Controllers
+{
+    public class StudentStatsController : Controller
+    {
+        private StudentStatsConnection db = new StudentStatsConnection();
+
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ TETSTETETE/Models/StudentStats.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ELearning.Models
+{
+    public class StudentStats
+    {
+        public int userID { get; set; }
+        public int Points { get; set; }
+    }
+
+    public class StudentStatsConnection : DbContext
+    {
+        public DbSet<StudentStats> StudentStats { get; set; }
+    }
+}
