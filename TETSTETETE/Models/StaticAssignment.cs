f2950615e061cc7d5871c2fd21fb14a12c8c4019
Henrik Hoeg <hotdacore@hotmail.com>
2012-05-09 17:13:23 +0200

    Fixed SQL injection vulnerability
    Signed-off-by: Henrik Hoeg <hotdacore@hotmail.com>

--- TETSTETETE/Models/StaticAssignment.cs
+++ TETSTETETE/Models/StaticAssignment.cs
@@ -35,11 +35,16 @@ namespace ELearning.Models
             using (MySqlConnection con = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString))
             {
                 con.Open();
-                var cmd = new MySqlCommand("INSERT INTO studentassignment VALUES ('LOL', 2, 'ROFL', 1, 1, 1, true, true, true, null)", con);
-                //cmd.Parameters.AddWithValue("id", null); //Null because we want database to generate primary key
-                //cmd.Parameters.AddWithValue("name", name.ToString());
-                //cmd.Parameters.AddWithValue("year", year.ToString());
-
+                var cmd = new MySqlCommand("INSERT INTO studentassignment VALUES (@username, @ID, @DateTime, @OptionTaken1, @OptionTaken2, @OptionTaken3, @Solved1, @Solved2, @Solved3, null)", con);
+                cmd.Parameters.AddWithValue("@UserName", "Henning");
+                cmd.Parameters.AddWithValue("@ID", 2);
+                cmd.Parameters.AddWithValue("@DateTime", "lol");
+                cmd.Parameters.AddWithValue("@OptionTaken1", 2);
+                cmd.Parameters.AddWithValue("@OptionTaken2", 3);
+                cmd.Parameters.AddWithValue("@OptionTaken3", 2);
+                cmd.Parameters.AddWithValue("@Solved1", true);
+                cmd.Parameters.AddWithValue("@Solved2", false);
+                cmd.Parameters.AddWithValue("@Solved3", true);
                 cmd.ExecuteNonQuery();
             }
         }
