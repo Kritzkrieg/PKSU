b7f351a7b4363281640cd6ca513b0390c839a110
Henrik Hoeg <hotdacore@hotmail.com>
2012-05-17 15:56:18 +0200

    Added method to get history for user

--- TETSTETETE/Controllers/AssignmentController.cs
+++ TETSTETETE/Controllers/AssignmentController.cs
@@ -36,6 +36,30 @@ namespace ELearning.Controllers
             return RedirectToAction("Result", "Assignment", mdl);
         }
 
+        //Get a specific assignment and the user's choices based on time of completetion
+        public ActionResult HistoryResult(Assignment assignment, string username, string datetime)
+        {
+            GivenAssignment mdl = new GivenAssignment();
+            mdl.gAssignment = assignment;
+            using (MySqlConnection con = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString))
+            {
+                con.Open();
+                var cmd = new MySqlCommand("SELECT OptionTaken1, OptionTaken2, OptionTaken3 FROM studentassignment WHERE UserName = @UserName AND ID = @ID AND DateTime = @DateTime", con);
+                cmd.Parameters.AddWithValue("@UserName", username);
+                cmd.Parameters.AddWithValue("@ID", assignment.ID);
+                cmd.Parameters.AddWithValue("@DateTime", datetime);
+                using (var r = cmd.ExecuteReader())
+                {
+                    r.Read();
+                    mdl.TheoryAnswer = r.GetInt32(0);
+                    mdl.Answer = r.GetInt32(1);
+                    mdl.FinalAnswer = r.GetInt32(2);
+                    
+                }
+            }
+                return RedirectToAction("Result", mdl);
+        }
+
         public ActionResult Result(ELearning.Models.GivenAssignment mdl)
         {
             return View(mdl);
